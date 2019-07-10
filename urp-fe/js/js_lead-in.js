b9562fa44b3d3440e0be659d8fba3820275ba16c
WangXiaoXin <wxx1_2_3@163.com>
2019-07-10 21:32:52 +0800

    first commit

---
+++ urp-fe/js/js_lead-in.js
@@ -0,0 +1,8 @@
+document.write('\
+<link rel="stylesheet" href="css/vant.css">\
+<script type="text/javascript" src="js/vue.min.js"></script>\
+<script type="text/javascript" src="js/vue-clipboard.min.js"></script>\
+<script type="text/javascript" src="js/vue-resource.min.js"></script>\
+<script type="text/javascript" src="js/vant.min.js"></script>\
+<script type="text/javascript" src="js/config.js"></script>\
+        ');
