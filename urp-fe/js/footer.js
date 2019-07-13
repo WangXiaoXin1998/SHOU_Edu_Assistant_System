8c713d0f6d3406e4eb4e54b76737e8547c72925c
WangXiaoXin <wxx1_2_3@163.com>
2019-07-13 17:33:27 +0800

    update login

---
+++ urp-fe/js/footer.js
@@ -0,0 +1,8 @@
+document.write('\
+<van-tabbar v-model="footeractive" fixed>\
+<van-tabbar-item icon="home-o" url="index.html">首页</van-tabbar-item>\
+<van-tabbar-item icon="todo-list-o" url="table.html">课表</van-tabbar-item>\
+<van-tabbar-item icon="records" url="score.html">成绩</van-tabbar-item>\
+<van-tabbar-item icon="user-o" url="myinfor.html">我的</van-tabbar-item>\
+</van-tabbar>\
+        ');
--- urp-fe/js/js_lead-in.js
+++ urp-fe/js/js_lead-in.js
@@ -4,5 +4,7 @@ document.write('\
 <script type="text/javascript" src="js/vue-clipboard.min.js"></script>\
 <script type="text/javascript" src="js/vue-resource.min.js"></script>\
 <script type="text/javascript" src="js/vant.min.js"></script>\
+<script type="text/javascript" src="js/axios.min.js"></script>\
+<script type="text/javascript" src="js/qs.min.js"></script>\
 <script type="text/javascript" src="js/config.js"></script>\
         ');
