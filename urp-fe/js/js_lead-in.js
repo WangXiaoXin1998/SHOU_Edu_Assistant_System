7765b1408f95de871105acb483a5d007dbba8f19
WangXiaoXin <wxx1_2_3@163.com>
2019-07-15 21:37:50 +0800

    bug fix

--- urp-fe/js/js_lead-in.js
+++ urp-fe/js/js_lead-in.js
@@ -1,4 +1,5 @@
 document.write('\
+<link rel="stylesheet" href="css/style.css">\
 <link rel="stylesheet" href="css/vant.css">\
 <script type="text/javascript" src="js/vue.min.js"></script>\
 <script type="text/javascript" src="js/vue-clipboard.min.js"></script>\
